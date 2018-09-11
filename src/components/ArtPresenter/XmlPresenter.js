0e225009896c69af6212666eb831f6f8c05d96dc
Fredrik F Lindhagen <fred.lindh96@gmail.com>
2018-09-11 10:37:13 +0200

    Closes #7 Add warning comments for innerHTML

--- src/components/ArtPresenter/XmlPresenter.js
+++ src/components/ArtPresenter/XmlPresenter.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// function parseSVG(svgString) {
-//   const parser = new DOMParser();
-//
-//   return parser.parseFromString(svgString, 'image/svg+xml');
-// }
-
 const XmlPresenter = ({ xmlString = null, ...props }) => {
   if (!xmlString) {
     return null;
   }
 
+  // Rendering svg through setting innerHTML is an option,
+  // but to prevent XSS should be try to find a better solution
   return <div {...props} dangerouslySetInnerHTML={{ __html: xmlString }} />;
 };
 
